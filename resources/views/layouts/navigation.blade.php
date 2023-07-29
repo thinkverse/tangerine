62284bf27bf4e32388c8c818effebb8e4a4c6321
Kim Hallberg <hallberg.kim@gmail.com>
2023-07-30 01:45:06 +0200

    Change application logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ url('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block h-9 w-auto text-center text-4xl" />
                     </a>
                 </div>
 
