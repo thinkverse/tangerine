556f6e83b7c28150ce5347138facd30a924a0db2
Kim Hallberg <hallberg.kim@gmail.com>
2023-07-30 01:33:49 +0200

    Convert routes to Folio pages

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,3 +1,11 @@
+<?php
+
+use function Laravel\Folio\middleware;
+
+middleware(['auth', 'verified']);
+
+?>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
