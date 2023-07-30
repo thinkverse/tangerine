7ea40b1e2b2e8f6fa6c40ca1058d31e1b4a01778
Kim Hallberg <hallberg.kim@gmail.com>
2023-07-30 17:31:22 +0200

    Make App navigation a Livewire component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            <livewire:navigation />
 
             <!-- Page Heading -->
             @if (isset($header))
