53bb4f987cb9bd620fe2ae0aa4571f1a06a52355
Kim Hallberg <hallberg.kim@gmail.com>
2023-07-30 02:13:50 +0200

    Remove unused import

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,7 +11,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
-use Illuminate\View\View;
 
 class RegisteredUserController extends Controller
 {
