19f3a1dd58fb8cfb10c843f3e0dcaf14923bcc8c
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-15 18:04:29 +0200

    Remove email verification middleware overwrite

--- resources/views/pages/verify-email.blade.php
+++ resources/views/pages/verify-email.blade.php
@@ -1,3 +1,11 @@
+<?php
+
+use function Laravel\Folio\name;
+
+name('verification.notice');
+
+?>
+
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
